9e256b8fd8954a622dcad0ea1aa7f3e19f485446
Stephen Flowers <sflower0@chicagobooth.edu>
2017-04-16 15:12:53 -0500

    lots of updates
    lat lng and forecast of lat lng works

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,17 +16,22 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+    wurl = "https://api.darksky.net/forecast/c2e79377963772be917398a84fdee0b6/#{@lat},#{@lng}"
+    parsed_wdata = JSON.parse(open(wurl).read)
+    #latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    #longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
+    current_temp = parsed_wdata["currently"]["temperature"]
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = current_temp
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = parsed_wdata["currently"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_wdata["minutely"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_wdata["hourly"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_wdata["daily"]["summary"]
 
     render("forecast/coords_to_weather.html.erb")
   end
--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -15,16 +15,15 @@ class MeteorologistController < ApplicationController
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
 
-
-
-    @current_temperature = "Replace this string with your answer."
-
+    
+    @current_temperature = current_temp
+#current_sum = parsed_wdata[]
     @current_summary = "Replace this string with your answer."
-
+#next_sixtymin = parsed_wdata[]
     @summary_of_next_sixty_minutes = "Replace this string with your answer."
-
+#next_sevhours = parsed_wdata[]
     @summary_of_next_several_hours = "Replace this string with your answer."
-
+#next_sevdays = parsed_wdata[]
     @summary_of_next_several_days = "Replace this string with your answer."
 
     render("meteorologist/street_to_weather.html.erb")
