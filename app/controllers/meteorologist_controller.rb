450e5b85d17821a86b3a1be7f6e214dfb0e7f42f
Stephen Flowers <sflower0@chicagobooth.edu>
2017-04-16 19:24:04 -0500

    tried to add font awesome
    website still works

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -14,17 +14,24 @@ class MeteorologistController < ApplicationController
     #
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@street_address}"
+    parsed_data = JSON.parse(open(url).read)
+    latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    @lat = latitude
+    @lng = longitude
+    wurl = "https://api.darksky.net/forecast/c2e79377963772be917398a84fdee0b6/#{@lat},#{@lng}"
+    parsed_wdata = JSON.parse(open(wurl).read)
 
-    
-    @current_temperature = current_temp
+    @current_temperature = parsed_wdata["currently"]["temperature"]
 #current_sum = parsed_wdata[]
-    @current_summary = "Replace this string with your answer."
-#next_sixtymin = parsed_wdata[]
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
-#next_sevhours = parsed_wdata[]
-    @summary_of_next_several_hours = "Replace this string with your answer."
-#next_sevdays = parsed_wdata[]
-    @summary_of_next_several_days = "Replace this string with your answer."
+@current_summary = parsed_wdata["currently"]["summary"]
+
+@summary_of_next_sixty_minutes = parsed_wdata["minutely"]["summary"]
+
+@summary_of_next_several_hours = parsed_wdata["hourly"]["summary"]
+
+@summary_of_next_several_days = parsed_wdata["daily"]["summary"]
 
     render("meteorologist/street_to_weather.html.erb")
   end
